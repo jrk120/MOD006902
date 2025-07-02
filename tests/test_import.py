b9da64c6def96a2ff28ec51358c00d1f586d1cf3
Jacob Knight <jrk120@student.aru.ac.uk>
2025-07-02 16:54:05 +0100

    Addition of testing and error handling

---
+++ tests/test_import.py
@@ -0,0 +1,78 @@
+#Import necessary modules
+import pytest
+import os
+from pyspark.testing.utils import assertSchemaEqual, assertDataFrameEqual
+from pyspark.sql.types import StructType, StructField, LongType, DoubleType, StringType
+from pyspark.sql import SparkSession
+
+#---------------------------------------------pre define repeated parameters for test------------------------------------
+
+@pytest.fixture
+def spark_fixture():
+    spark = SparkSession.builder.appName("Testing PySpark Example").getOrCreate()
+    yield spark
+
+@pytest.fixture
+def expectedSchema(spark_fixture):
+    expectedSchema = StructType([
+        StructField("gameId", LongType(), True),
+        StructField("playId", LongType(), True),
+        StructField("nflId", DoubleType(), True),
+        StructField("frameId", LongType(), True),
+        StructField("x", DoubleType(), True),
+        StructField("y", DoubleType(), True),
+        StructField("frameType", StringType(), True)
+    ])
+    return expectedSchema
+
+@pytest.fixture
+def trackingDf(spark_fixture, expectedSchema):
+    folder = r"C:\Users\knigh\MOD006902\data\tracking"
+    files = [os.path.join(folder, f) for f in os.listdir(folder) if f.endswith(".parquet")]
+    return spark_fixture.read.schema(expectedSchema).parquet(*files)
+
+
+#------------------------------------------------------Test functions----------------------------------------------------------
+def test_tracking_import_schema(spark_fixture, expectedSchema, trackingDf):
+    '''
+    This function checks if there are missing columns or schema drift for future reuse
+    '''
+    assertSchemaEqual(trackingDf.schema, expectedSchema, ignoreNullable=True)
+
+
+def test_if_columns_have_nulls(spark_fixture, expectedSchema, trackingDf):
+    '''
+    This function checks to see if any of the columns are null
+    '''
+    expectedColumns = [field.name for field in expectedSchema.fields]
+    nulls = [column for column in expectedColumns if trackingDf.select(column).distinct().count() == 1 and trackingDf.select(column).first()[0] is None]
+    print(f"Null columnns: {nulls}")
+    assert not nulls
+
+def test_parquet_conversion(spark_fixture):
+    truthCSVs = r"C:\Users\knigh\MOD006902\tests\test_data"
+    convertedData = r"C:\Users\knigh\MOD006902\data"
+
+    files = ["games", "player_play", "players", "plays"]
+    csvs = {}
+    parqeuts = {}
+    for file in files:
+        csvs[file] = spark_fixture.read.option("header", True).option("inferSchema", True).csv(truthCSVs + "/" + file + ".csv")
+        parqeuts[file] = spark_fixture.read.parquet(convertedData + "/" + file + ".parquet")
+
+    for file, CSVDf in csvs.items():
+        from pyspark.sql.functions import to_timestamp, date_format, col
+
+        parqeutDf = parqeuts[file]
+
+        if file == "games":
+            parqeutDf = parqeutDf.withColumn("gameTimeEastern",date_format(to_timestamp(col("gameTimeEastern"), "H:mm:ss"), "HH:mm:ss"))
+            CSVDf = CSVDf.withColumn("gameTimeEastern",date_format(to_timestamp(col("gameTimeEastern"), "H:mm:ss"), "HH:mm:ss"))
+
+        actual_rows = set([tuple(row) for row in CSVDf.collect()])
+        expected_rows = set([tuple(row) for row in parqeutDf.collect()])
+
+        assert actual_rows == expected_rows, "DataFrame values do not match"
+    
+
+    
